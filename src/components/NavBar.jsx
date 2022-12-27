90e72fb7c68b63e8a11b2faf3bcf03f8650084f1
Chris Mems <christopher.mems@.com>
2022-12-27 13:25:09 -0600

    create navbar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react'
+import {FaBars, FaFacebookF, FaTwitter, FaGooglePlusG, FaInstagram,} from 'react-icons/fa'
+import {BsChatSquareDots} from 'react-icons/bs'
+
+
+function NavBar() {
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
