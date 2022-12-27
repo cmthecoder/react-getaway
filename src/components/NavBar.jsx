e7f61bc3daffe022e0a7caf07882cd69b0b10b8d
Chris Mems <christopher.mems@.com>
2022-12-27 14:40:49 -0600

    toggle hamburger nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,15 @@ import {BsChatSquareDots} from 'react-icons/bs'
 
 
 function NavBar() {
+  const [nav, setNav] = useState(false)
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
   return (
-    <div>
-      <ul>
+    <div className='w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-gray-700/80'>
+      <ul className='hidden sm:flex px-4'>
         <li>
           <a href="/">Home</a>
         </li>
@@ -20,11 +26,33 @@ function NavBar() {
           <a href="#contact">Contact</a>
         </li>
       </ul>
-      <div>
+      <div className='flex jsutify-between'>
         <FaFacebookF className='mx-4' />
         <FaTwitter className='mx-4' />
         <FaGooglePlusG className='mx-4' />
         <FaInstagram className='mx-4' />
+      </div>
+      <div onClick={handleNav} className='sm:hidden z-10'>
+        <FaBars size={20} classNamemr-4 cursor-pointer/>
+      </div>
+      <div onClick={handleNav} className={
+        nav
+        ? 'overflow-y-hidden md:hidden ease-in duration-300 absolute text-gray-300 left-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex flex flex-col'
+        : 'absolute top-0 h-screen left-[-100%] ease-in duration-500'}>
+        <ul className='h-full w-full text-center pt-12'>
+          <li className='text-2xl py-8'>
+            <a href="/">Home</a>
+          </li>
+          <li className='text-2xl py-8'>
+            <a href="#gallery">Gallery</a>
+          </li>
+          <li className='text-2xl py-8'>
+            <a href="#deals">Deals</a>
+          </li>
+          <li className='text-2xl py-8'>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
       </div>
     </div>
   )
