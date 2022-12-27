4ee0373264d8e8a0a06440970a02451cd2fc46a8
Chris Mems <christopher.mems@.com>
2022-12-27 15:50:33 -0600

    add css for activities

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 function Activities() {
   return (
-    <div>Activities</div>
+    <div className='max-w-[1140px] m-auto w-full md:flex mt-[-75px]'>
+      <div className='relative p-4'>
+        <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Resorts</h3>
+        <img className='w-full h-full object-cover relative border-4 border-white shadow-lg' src="https://i.imgur.com/OrAYBC9.jpg" alt="/" />
+      </div>
+      <div className='relative p-4'>
+        <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Resorts</h3>
+        <img className='w-full h-full object-cover relative border-4 border-white shadow-lg' src="https://i.imgur.com/OrAYBC9.jpg" alt="/" />
+      </div>
+      <div className='relative p-4'>
+        <h3 className='absolute z-10 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-2xl font-bold'>Resorts</h3>
+        <img className='w-full h-full object-cover relative border-4 border-white shadow-lg' src="https://i.imgur.com/OrAYBC9.jpg" alt="/" />
+      </div>
+    </div>
   )
 }
 
